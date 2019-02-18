4388196a019105ce7673d494ada049af04fb2496
1831241 <1831241@LABCMAQ08.itp.local>
2019-02-18 11:46:41 -0300

    listasPW2

---
+++ duda/lista1.php
@@ -0,0 +1,22 @@
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$n1=10;
+$n2=4;
+$n3=8;
+$n4=6; 
+$soma=($n1+$n2+$n3+$n4)/4;
+if ($soma >=6){
+    echo "aprovado";
+   
+}else {
+    echo "reprovado";
+}
+?>
+
+
